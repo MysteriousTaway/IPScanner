16a496955338c20fcdb2e0f6fa6517afbb15f943
Taway <MysteriousTaway@outlook.com>
2022-05-17 16:03:42 +0200

    now works

--- IPScanner/Program.cs
+++ IPScanner/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
@@ -6,125 +8,34 @@ using System.Text;
 
 namespace IPScanner {
     internal class Program {
-        private static int port = 8728;
-        
-        private static int maskInt;
-
-        private static long original;
-        private static long current;
-        private static long maskLong;
-        
+        // private static int port = 8728;
         public static void Main(string[] args) {
-            try {
-                // Get IP from user:
-                Console.WriteLine("Valid IP format: 192.168.0.1");
-                original = IPStringToLong(Console.ReadLine());
-                
-                // Get mask from user:
-                Console.WriteLine("Valid mask format: 24");
-                maskInt = Int32.Parse(Console.ReadLine());
-                maskLong = GetMaskLong(original, maskInt);
-                // Get first ip address long:
-                Console.WriteLine("---------------------------");
-                Console.WriteLine("maskLong: " + maskLong);
-                GetFirstAddressLong(original, maskLong);
-            } catch (Exception exception) {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Internal error occured: {0}", exception);
-                Console.ForegroundColor = ConsoleColor.White;
-                Main(args);
-            }
-        }
-
-        static long GetFirstAddressLong(long ip, long mask) {
-            Console.WriteLine("ip: " + IPLongToString(ip));
-            Console.WriteLine("mask: " + IPLongToString(mask));
-            
-            String ipBin = StringToBinary(IPLongToString(ip));
-            String maskBin = StringToBinary(IPLongToString(mask));
-            
-            Console.WriteLine("ipBin: " + ipBin);
-            Console.WriteLine("maskBin: " + maskBin);
-            return 0;
-        }
-        
-        private static string StringToBinary(string data) {
-            StringBuilder sb = new StringBuilder();
- 
-            foreach (char c in data.ToCharArray())
-            {
-                sb.Append(Convert.ToString(c, 2).PadLeft(8, '0'));
-            }
-            return sb.ToString();
-        }
-
-        static long GetMaskLong(long ip , int mask) {
-            return IPStringToLong(GetBroadcastAddressString(new IPAddress(ip), GetMaskFromInt(mask)));
-        }
-
-        static IPAddress GetMaskFromInt(int maskInt) {
-            int remaining = 32-maskInt;
-            String ipBin = "";
-            
-            for (int i = 0; i < maskInt; i++) { ipBin += "1"; }
-            for (int j = 0; j < remaining; j++) { ipBin += "0"; }
-
-            string a = Convert.ToInt32(ipBin.Substring(0, 8), 2).ToString();
-            string b = Convert.ToInt32(ipBin.Substring(8, 8), 2).ToString();
-            string c = Convert.ToInt32(ipBin.Substring(16, 8), 2).ToString();
-            string d = Convert.ToInt32(ipBin.Substring(24, 8), 2).ToString();
-            
-            String ip = a + "." + b + "." + c + "." + d;
-            return IPAddress.Parse(ip);
-        }
-
-        static String ConvertMaskIntString(int maskInt) {
-            int remaining = 32-maskInt;
-            String ipBin = "";
-            
-            for (int i = 0; i < maskInt; i++) { ipBin += "1"; }
-            for (int j = 0; j < remaining; j++) { ipBin += "0"; }
-            return ipBin.Substring(0, 8) + "." + ipBin.Substring(7, 8) + "." + ipBin.Substring(15, 8) + "." + ipBin.Substring(23, 8);
-        }
-        
-        private static IPAddress GetBroadcastAddress(IPAddress address, IPAddress mask) {
-            uint ipAddress = BitConverter.ToUInt32(address.GetAddressBytes(), 0);
-            uint ipMaskV4 = BitConverter.ToUInt32(mask.GetAddressBytes(), 0);
-            uint broadCastIpAddress = ipAddress | ~ipMaskV4;
-
-            return new IPAddress(BitConverter.GetBytes(broadCastIpAddress));
-        }
-        
-        private static String GetBroadcastAddressString(IPAddress address, IPAddress mask) {
-            uint ipAddress = BitConverter.ToUInt32(address.GetAddressBytes(), 0);
-            uint ipMaskV4 = BitConverter.ToUInt32(mask.GetAddressBytes(), 0);
-            uint broadCastIpAddress = ipAddress | ~ipMaskV4;
-
-            return Convert.ToString(broadCastIpAddress);
-        }
-
-        static long IPStringToLong(string addr) {
-            return (long) (uint) IPAddress.NetworkToHostOrder((int) IPAddress.Parse(addr).Address);
-        }
-
-        static string IPLongToString(long address) { 
-            Console.WriteLine( IPAddress.Parse(address.ToString()).ToString() + "/" + address.ToString());
-            return IPAddress.Parse(address.ToString()).ToString();
-        }
-
-        static bool CanPingIP(String ipAddr) {
-            Ping pingSender = new Ping ();
-            PingOptions options = new PingOptions ();
-            options.DontFragment = true;
-            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-            byte[] buffer = Encoding.ASCII.GetBytes (data);
-            int timeout = 1;
-            PingReply reply = pingSender.Send(ipAddr, timeout, buffer, options);
-            if (reply.Status == IPStatus.Success) {
-                Console.WriteLine ("RoundTrip time: {0}", reply.RoundtripTime);
-                return true;
-            }
-            return false;
+            List<Subnet> subnets = new List<Subnet>();
+            // try {
+            //     // Get IP from user:
+            //     Console.WriteLine("Valid IP format: 192.168.0.1");
+            //     string ip = Console.ReadLine();
+            //     
+            //     // Get mask from user:
+            //     int mask;
+            //     do {
+            //         Console.WriteLine("Valid mask format: 24");
+            //         mask = Int32.Parse(Console.ReadLine());
+            //     } while (mask < 0 || mask > 32);
+            //     
+            //     // Get port:
+            //     Console.WriteLine("Valid port format: 8728");
+            //     int port = Int32.Parse(Console.ReadLine());
+            //     subnets.Add(new Subnet(ip, mask, port));
+            //     
+            // } catch (Exception exception) {
+            //     Console.ForegroundColor = ConsoleColor.Red;
+            //     Console.WriteLine("Internal error occured: {0}", exception);
+            //     Console.ForegroundColor = ConsoleColor.White;
+            //     Main(args);
+            // }
+            subnets.Add(new Subnet("192.168.255.0", 24, 8728, 100));
+            subnets[0].Run();
         }
     }
 }
---
+++ IPScanner/Subnet.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace IPScanner {
+    public class Subnet {
+        // Vars:
+        public string ip;
+        public int mask;
+
+        // long of ^
+        public long ipLong;
+        public long maskLong;
+
+        public string networkIP;
+        public string broadcastIP;
+        
+        // Port to which it's connecting:
+        public int port;
+        
+        // the whole subnet thingy:
+        public long networkIPLong;
+        public long broadcastIPLong;
+
+        public int msTimeout;
+
+        public Subnet(string ip, int mask, int port, int msTimeout) {
+            this.ip = ip;
+            this.mask = mask;
+            this.port = port;
+            this.msTimeout = msTimeout;
+            
+            ipLong = ConvertIpToLong(ip);
+            networkIP = GetNetworkIPLong(ip, mask);
+            broadcastIP = GetBroadcastIPLong(ip, mask);
+            networkIPLong = ConvertIpToLong(networkIP);
+            broadcastIPLong = ConvertIpToLong(broadcastIP);
+        }
+
+        public void Run() {
+            for (long i = networkIPLong; i <= broadcastIPLong; i++) {
+                try {
+                    String currentIP = ConvertLongToIp(i);
+                    // Console.WriteLine(currentIP);
+                    Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    // socket.ReceiveTimeout = 500;
+                    // socket.Connect(currentIP, port);
+                    IAsyncResult result = socket.BeginConnect( currentIP, port, null, null );
+                    bool success = result.AsyncWaitHandle.WaitOne( msTimeout, true );
+                    if (success) {
+                        Console.WriteLine("Can connect to: " + currentIP);
+                        socket.Disconnect(false);
+                    }
+                } catch (Exception e) {}
+            }
+        }
+        
+        #region IP conversion
+
+        string GetNetworkIPLong(String ip, int mask) {
+            String maskBinary = ConvertMaskBinaryToString(mask);
+            long ipLong = ConvertIpToLong(ip);
+            long maskLong = ConvertIpToLong(maskBinary);
+            return ConvertLongToIp(ipLong & maskLong);
+        }
+        
+        string GetBroadcastIPLong(String ip, int mask) {
+            String maskBinary = ConvertMaskBinaryToString(mask);
+            long ipLong = ConvertIpToLong(ip);
+            long maskLong = ConvertIpToLong(maskBinary);
+            
+            return ConvertLongToIp(ipLong | ~maskLong);
+        }
+        
+        long ConvertIpToLong(String ip) {
+            String[] split = ip.Split(Char.Parse("."));
+            int[] intSplit = new int[4];
+            for (int i = 0; i < split.Length; i++) {
+                intSplit[i] = Int32.Parse(split[i]);
+            }
+
+            intSplit[0] *= (int)Math.Pow(2, 24);
+            intSplit[1] *= (int)Math.Pow(2, 16);
+            intSplit[2] *= (int)Math.Pow(2, 8);
+            intSplit[3] *= 1;
+            return intSplit[0] + intSplit[1] + intSplit[2] + intSplit[3];
+        }
+
+        string ConvertLongToIp(long ip) {
+            long[] split = new long[4];
+            
+            split[0] = (ip >> 24) & 0xFF;
+            split[1] = (ip >> 16) & 0xFF;
+            split[2] = (ip >> 8) & 0xFF;
+            split[3] = (ip >> 0) & 0xFF;
+            
+            return split[0] + "." + split[1] + "." + split[2] + "." + split[3];
+        }
+
+        #endregion
+
+        #region Mask conversion
+
+        string ConvertMaskBinaryToString(int mask) {
+            long mask1 = 0xFFFFFFFFF << 32 - mask;
+            long[] maskArr = new long[4];
+            maskArr[0] = (mask1 >> 24) & 0xFF;
+            maskArr[1] = (mask1 >> 16) & 0xFF;
+            maskArr[2] = (mask1 >> 8) & 0xFF;
+            maskArr[3] = (mask1 >> 0) & 0xFF;
+            return maskArr[0] + "." + maskArr[1] + "." + maskArr[2] + "." + maskArr[3];
+        }
+
+        #endregion
+    }
+}
